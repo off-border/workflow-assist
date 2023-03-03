d569c5451fc6f0719d30a803d495cdff1f21cc6b
off_border <workflow-github@alpah.ru>
2023-03-03 23:37:14 +0700

    refactored

--- workflow.js
+++ workflow.js
@@ -5,17 +5,16 @@ import { createCommands } from './commands.js';
 
 
 export function workflow ({ config, utils }, cmdLine) {
-        const { parseCmdLine } = createHelpers({ config, utils })
-        const { startTask } = createCommands({ config, utils });
-
-        const [cmd, ...args] = parseCmdLine(cmdLine);
-
-        const handlers = {
-            start: startTask,
-        };
-
         utils.createDirNotExist(config.rootDir);
         utils.setWorkingDir(config.rootDir);
 
-        handlers[cmd](...args);
+        const { parseCmdLine } = createHelpers({ config, utils })
+        const [cmd, ...args] = parseCmdLine(cmdLine);
+        
+        const { startTask } = createCommands({ config, utils });
+        const commands = {
+            start: startTask,
+        };
+
+        commands[cmd](...args);
     };
