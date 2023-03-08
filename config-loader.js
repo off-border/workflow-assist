5c8550c74879a9d77ec0a63351e31640eeeffb80
off_border <workflow-github@alpah.ru>
2023-03-08 18:28:24 +0700

    config loader works

--- config-loader.js
+++ config-loader.js
@@ -1,9 +1,11 @@
+import { createRequire } from 'module';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const CONFIG_NAME = '.workflow.config.js';
 
-export function loadConfig(cwd = process.cwd(), requireFile = require) {
-    const config = searchInTree(cwd, CONFIG_NAME, requireFile);
+export async function loadConfig(cwd = process.env.PWD, requireFile) {
+    const config = await searchInTree(cwd, CONFIG_NAME, requireFile);
 
     if (config) {
         return config;
@@ -12,21 +14,50 @@ export function loadConfig(cwd = process.cwd(), requireFile = require) {
     throw new Error('CONFIG NOT FOUND');
 }
 
-function searchInTree(dir, file, requireFile) {
+async function searchInTree(dir, file, requireFile) {
     let pathArr = dir.split('/');
     let config;
     while (!config && pathArr.length) {
-        config = tryRequire(pathArr, CONFIG_NAME, requireFile);
+        config = await tryRequire(pathArr, CONFIG_NAME, requireFile);
         pathArr.pop();
     }
 
     return config;
 }
 
-function tryRequire(dirArr, file, requireFile) {
+async function tryRequire(dirArr, file, requireFile) {
+    if (requireFile) {
+        try {
+            return requireFile(path.join(...dirArr, file));
+        } catch (e) {
+            return null;
+        }
+    }
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const absolutePath = path.join('/', ...dirArr, file);
+    const basePath = __dirname;
+    let relativePath = path.relative(basePath, absolutePath);
+
+    console.log('\n---fullpath', absolutePath);
+    console.log('---basePath', basePath);
+    console.log('---relativePath', relativePath);
+
+    if (relativePath === '.workflow.config.js') {
+        relativePath = './.workflow.config.js';
+    }
+
     try {
-        return requireFile(path.join(...dirArr, file));
+        const modul = await import(relativePath);
+        console.log('---loaded module:', modul);
+
+        return modul.default;
+
+        // return createRequire(basePath)(relativePath);
     } catch (e) {
+        console.log('---- not found in:', relativePath);
         return null;
     }
 }
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import { loadConfig } from './config-loader.js';
 import { createApi } from './api/api.js';
 import { workflow } from './workflow.js';
 
-const config = loadConfig();
-const api = createApi({ config });
-const cmd = process.argv.slice(2).join(' ');
+async function run() {
+    const config = await loadConfig();
+    console.log('---config', config);
+    const api = createApi({ config });
+    const cmd = process.argv.slice(2).join(' ');
+    workflow({ config, api }, cmd);
+}
 
-workflow({ config, api }, cmd);
+run();
