47be9f8b5759b67fe6e1209771e66bf148597b22
off_border <workflow-github@alpah.ru>
2023-12-28 18:28:49 +0700

    wofo2: added "show config | task" command

--- wofo2/index.ts
+++ wofo2/index.ts
@@ -1,6 +1,10 @@
-import { program } from 'commander';
+import { Argument, program } from 'commander';
 import { startNewTask } from './commands/start.js';
 import { commit } from './commands/commit.js';
+import { loadConfig } from './config-loader.js';
+import { info } from './api/msg.js';
+import { getTaskIdFromBranch } from './api/task.js';
+import { getCurrentBranch } from './api/git.js';
 
 program.name('wofo2').version('1.0.0');
 
@@ -10,13 +14,40 @@ program
         'start new task (create working copy, task branch, and install deps)'
     )
     .argument('<taskId>', 'JIRA task id')
-    .action(startNewTask);
+    .action(startNewTask)
+    .showHelpAfterError();
+
 
 program
     .command('commit')
     .description('commit changes to task branch')
     .argument('<type>', 'type of the commit (fix, feat, refc, etc')
     .argument('<message...>', 'commit message')
-    .action(commit);
+    .action(commit)
+    .showHelpAfterError();
+
+program
+    .command('show')
+    .description('show additional info')
+    .addArgument(
+        new Argument('<topic>', 'topic to show').choices(['config', 'task'])
+    )
+    .action(async (topic) => {
+        const config = await loadConfig();
+
+        switch (topic) {
+            case 'config':
+                info('current config:');
+                console.log(config);
+                return;
+            case 'task':
+                const currentBranch = getCurrentBranch();
+                const taskId = await getTaskIdFromBranch(currentBranch || '');
+                info('current task:');
+                console.log(taskId);
+                return;
+        }
+    })
+    .showHelpAfterError();
 
 program.parse(process.argv);
