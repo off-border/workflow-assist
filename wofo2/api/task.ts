d8737b59c6d2ab51509e645a991a8e8653ea1db4
off_border <workflow-github@alpah.ru>
2023-12-28 17:45:22 +0700

    start: fixed missed lowercase config

--- wofo2/api/task.ts
+++ wofo2/api/task.ts
@@ -1,6 +1,7 @@
 import { join } from "path";
 import { loadConfig } from "../config-loader.js";
 import { resolvePath } from "./fs.js";
+import { error } from "./msg.js";
 
 async function getTaskDir(taskId: string) {
     const config = await loadConfig();
@@ -15,10 +16,34 @@ async function getTaskDir(taskId: string) {
     return taskDir;
 }
 
+async function getTaskIdFromBranch(branch: string) {
+    const config = await loadConfig();
+    const extractRegex = config.commit.taskId.extractRegex;
+
+    if (!extractRegex) {
+        return branch;
+    }
+
+    const extractedTaskId = branch.match(extractRegex)?.[0];
+
+    if (!extractedTaskId) {
+        error(`Failed to extract taskId from branch: ${branch}`);
+        throw new Error('Failed to extract taskId from branch');
+    }
+
+    const taskId = config.commit.taskId.upperCase
+        ? extractedTaskId.toUpperCase()
+        : extractedTaskId;
+
+    return taskId;
+
+}
+
 function getEscapedTaskId(taskId: string) {
     return taskId.replace(/\//g, '_');
 }   
 
 export {
     getTaskDir,
+    getTaskIdFromBranch,
 }
